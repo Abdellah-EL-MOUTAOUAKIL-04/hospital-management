4ba63dd8257a644a112ca5a403e57baf4fa41bd7
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-17 08:49:06 +0000

    init

---
+++ src/test/java/ma/abdellah/hospitalmanagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.abdellah.hospitalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
